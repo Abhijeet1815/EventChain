5483b6fd52b7dc54b311d8cb6fcc508f9362dbac
bitflipper19 <samyakpandey2007@gmail.com>
2025-12-31 01:36:36 +0530

    made login screen

--- lib/screens/auth/signIn/place_holder.dart
+++ lib/screens/auth/signIn/place_holder.dart
@@ -8,64 +8,57 @@ class placeHolder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: SizeUtils.height(context, 0.7),
-      width: SizeUtils.width(context, 0.9),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(SizeUtils.height(context, 0.03)),
-        boxShadow: [
-          BoxShadow(
-            color: const Color(0xFF717182),
-            offset: Offset(0, 5),
-            blurRadius: SizeUtils.height(context, 0.01),
-            spreadRadius: 0,
+    return Card(
+      elevation: 10,
+      child: Container(
+        height: SizeUtils.height(context, 0.7),
+        width: SizeUtils.width(context, 0.9),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(SizeUtils.height(context, 0.03)),
+          color: Color(0xFFFFFFFF),
+          border: BoxBorder.all(
+            color: Color(0xFFF3F4F6),
+            width: 0.5,
+            style: BorderStyle.solid,
           ),
-        ],
-        color: Color(0xFFFFFFFF),
-        border: BoxBorder.all(
-          color: Color(0xFFF3F4F6),
-          width: 0.5,
-          style: BorderStyle.solid,
         ),
-      ),
-      child: Center(
-        child: Column(
-          // spacing: SizeUtils.height(context, 0.0005),
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Row(
-              spacing: SizeUtils.width(context, 0.05),
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: SizeUtils.height(context, 0.15),
-                  width: SizeUtils.width(context, 0.15),
-                  child: Image.asset("assets/images/appLogo.png"),
+        child: Center(
+          child: Column(
+            // spacing: SizeUtils.height(context, 0.0005),
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: SizeUtils.height(context, 0.05)),
+              SizedBox(
+                height: SizeUtils.height(context, 0.08),
+                child: Row(
+                  spacing: SizeUtils.width(context, 0.05),
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: SizeUtils.height(context, 0.08),
+                      width: SizeUtils.width(context, 0.15),
+                      child: Image.asset("assets/images/appLogo.png"),
+                    ),
+                    UiHelper.customText(
+                      text: "EventChain",
+                      textColor: 0xFF9810FA,
+                      fontWeight: FontWeight.bold,
+                      fontSize: SizeUtils.text(context, 0.07),
+                    ),
+                  ],
                 ),
-                UiHelper.customText(
-                  text: "EventChain",
-                  textColor: 0xFF9810FA,
-                  fontWeight: FontWeight.bold,
-                  fontSize: SizeUtils.text(context, 0.07),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: SizeUtils.height(context, 0.01),
-              width: SizeUtils.width(context, 0.9),
-            ),
-            UiHelper.customText(
-              text: "Welcome back! Sign in to continue",
-              textColor: 0xFF717182,
-              fontWeight: FontWeight.normal,
-              fontSize: SizeUtils.text(context, 0.04),
-            ),
-            SizedBox(
-              height: SizeUtils.height(context, 0.02),
-              width: SizeUtils.width(context, 0.9),
-            ),
-            GoogleButton(),
-          ],
+              ),
+              SizedBox(height: SizeUtils.height(context, 0.01)),
+              UiHelper.customText(
+                text: "Welcome back! Sign in to continue",
+                textColor: 0xFF717182,
+                fontWeight: FontWeight.normal,
+                fontSize: SizeUtils.text(context, 0.04),
+              ),
+              SizedBox(height: SizeUtils.height(context, 0.05)),
+              GoogleButton(),
+            ],
+          ),
         ),
       ),
     );
